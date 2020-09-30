524a639789b621b437e3cc7bab3e38b7d51c8d98
Jimmy <7905351@qq.com>
2020-09-30 15:51:15 +0800

    update: items endpoints

--- app/api/api_v1/endpoints/items.py
+++ app/api/api_v1/endpoints/items.py
@@ -14,15 +14,16 @@ from app.api import deps
 router = APIRouter()
 
 
-@router.get("/", response_model=List[schemas.Item])
+@router.get("/", response_model=List[schemas.Item], summary="item列表")
 def read_items(
-    db: Session = Depends(deps.get_db),
-    skip: int = 0,
-    limit: int = 100,
-    current_user: models.User = Depends(deps.get_current_active_user),
+        db: Session = Depends(deps.get_db),
+        skip: int = 0,
+        limit: int = 100,
+        current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
-    Retrieve items.
+    Retrieve items.\n
+    管理员可以查看所有人的item，普通用户只可以查看自己的item
     """
     if crud.user.is_superuser(current_user):
         items = crud.item.get_multi(db, skip=skip, limit=limit)
@@ -33,12 +34,12 @@ def read_items(
     return items
 
 
-@router.post("/", response_model=schemas.Item)
+@router.post("/", response_model=schemas.Item, summary="新增item")
 def create_item(
-    *,
-    db: Session = Depends(deps.get_db),
-    item_in: schemas.ItemCreate,
-    current_user: models.User = Depends(deps.get_current_active_user),
+        *,
+        db: Session = Depends(deps.get_db),
+        item_in: schemas.ItemCreate,
+        current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
     Create new item.
@@ -47,18 +48,19 @@ def create_item(
     return item
 
 
-@router.put("/{id}", response_model=schemas.Item)
+@router.put("/{pk}", response_model=schemas.Item, summary="更新item")
 def update_item(
-    *,
-    db: Session = Depends(deps.get_db),
-    id: int,
-    item_in: schemas.ItemUpdate,
-    current_user: models.User = Depends(deps.get_current_active_user),
+        *,
+        db: Session = Depends(deps.get_db),
+        pk: int,
+        item_in: schemas.ItemUpdate,
+        current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
-    Update an item.
+    Update an item.\n
+    管理员可以更新所有人的item，普通用户只可以更新自己的item
     """
-    item = crud.item.get(db=db, id=id)
+    item = crud.item.get(db=db, id=pk)
     if not item:
         raise HTTPException(status_code=404, detail="Item not found")
     if not crud.user.is_superuser(current_user) and (item.owner_id != current_user.id):
@@ -67,17 +69,18 @@ def update_item(
     return item
 
 
-@router.get("/{id}", response_model=schemas.Item)
+@router.get("/{pk}", response_model=schemas.Item, summary="item详情")
 def read_item(
-    *,
-    db: Session = Depends(deps.get_db),
-    id: int,
-    current_user: models.User = Depends(deps.get_current_active_user),
+        *,
+        db: Session = Depends(deps.get_db),
+        pk: int,
+        current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
-    Get item by ID.
+    Get item by ID.\n
+    管理员可以获取所有人的item，普通用户只可以获取自己的item
     """
-    item = crud.item.get(db=db, id=id)
+    item = crud.item.get(db=db, id=pk)
     if not item:
         raise HTTPException(status_code=404, detail="Item not found")
     if not crud.user.is_superuser(current_user) and (item.owner_id != current_user.id):
@@ -85,20 +88,21 @@ def read_item(
     return item
 
 
-@router.delete("/{id}", response_model=schemas.Item)
+@router.delete("/{pk}", response_model=schemas.Item, summary="删除item")
 def delete_item(
-    *,
-    db: Session = Depends(deps.get_db),
-    id: int,
-    current_user: models.User = Depends(deps.get_current_active_user),
+        *,
+        db: Session = Depends(deps.get_db),
+        pk: int,
+        current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
-    Delete an item.
+    Delete an item.\n
+    管理员可以删除所有人的item，普通用户只可以删除自己的item
     """
-    item = crud.item.get(db=db, id=id)
+    item = crud.item.get(db=db, id=pk)
     if not item:
         raise HTTPException(status_code=404, detail="Item not found")
     if not crud.user.is_superuser(current_user) and (item.owner_id != current_user.id):
         raise HTTPException(status_code=400, detail="Not enough permissions")
-    item = crud.item.remove(db=db, id=id)
+    item = crud.item.remove(db=db, pk=pk)
     return item
